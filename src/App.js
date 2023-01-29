2e8cd843fd2cc367230531a5089235ca1596329d
Alison Estephany <alisonestephanyoz@gmail.com>
2023-01-29 18:04:35 -0400

    Add Routing functionality with React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//import pages 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div className="App h-screen bg-purple-400">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register />}/>
+          <Route path="protectedroute" element={<ProtectedRoute />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
