bd671733a0ae8c7711a920794188c89839f53a9c
Alison Estephany <alisonestephanyoz@gmail.com>
2023-01-29 22:35:57 -0400

    Add Form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
+
+  const [user, setUser] = useState({
+    email:"",
+    password:""
+  })
+
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleChange = (e) => {
+    setUser((prevProps)=> ({
+      ...prevProps,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleRegistration = (e) => {
+    e.preventDefault()
+    if (user.password !== confirmPassword) {
+      return alert("Passwords do not match");
+    }
+    
+  }
   return (
-    <div>Register</div>
+    <div>
+      <form onSubmit={handleRegistration}>
+        <div>
+          <label>Email</label>
+          <input 
+            name="email"
+            value={user.email}
+            type="email"
+            placeholder="enter your email"
+            onChange={handleChange}
+
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input 
+            name="password"
+            type="password"
+            value={user.password}
+            placeholder="Enter your password"
+            onChange={handleChange}
+          />
+          <label>Confirm Password</label>
+          <input 
+            name="password"
+            type="password"
+            placeholder="Please confirm your password"
+            onChange={(e)=> setConfirmPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button>Register</button>
+        </div>
+      </form>
+
+      <NavLink to="/login">Already have an account? Login!</NavLink>
+    </div>
   )
 }
 
