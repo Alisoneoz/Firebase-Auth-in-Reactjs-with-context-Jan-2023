0847a8a840fe755780090060d98b95fc0be6e7e2
Alison Estephany <alisonestephanyoz@gmail.com>
2023-01-30 19:44:31 -0400

    Add auth variable

--- src/firebaseConfiguration.js
+++ src/firebaseConfiguration.js
@@ -1,5 +1,5 @@
-
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
